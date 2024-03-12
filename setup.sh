02f1166de4e7379ebb3fbc57ee76223d4b40d3e5
Isaac <isaac.ault@codeplay.com>
2024-03-12 20:37:32 +0000

    Sync

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ if [[ -z "$XDG_CONFIG_HOME" ]]; then
     exit 1
 fi
 
+# don't use this
+# I think these are backwards rn
+
 ln -s $FLAG $DOTFILES_ROOT/zsh $HOME/.zsh
 ln -s $FLAG $DOTFILES_ROOT/zsh/zshrc $HOME/.zshrc
 ln -s $FLAG $DOTFILES_ROOT/zsh/p10k.zsh $HOME/.p10k.zsh
@@ -17,3 +20,5 @@ ln -s $FLAG $DOTFILES_ROOT/i3 $XDG_CONFIG_HOME/i3
 ln -s $FLAG $DOTFILES_ROOT/polybar $XDG_CONFIG_HOME/polybar
 ln -s $FLAG $DOTFILES_ROOT/nvim $XDG_CONFIG_HOME/nvim
 ln -s $FLAG $DOTFILES_ROOT/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $FLAG $DOTFILES_ROOT/waybar $XDG_CONFIG_HOME/waybar
+ln -s $FLAG $DOTFILES_ROOT/sway $XDG_CONFIG_HOME/sway
